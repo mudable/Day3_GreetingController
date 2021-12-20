9b769d80b97dea95f1ed31b6887e7916eadb5e61
mudable <mudableshilpa@gmail.com>
2021-12-20 17:12:45 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/greeting/GreetingController.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	  private static final String template = "Hello, %s!";
+      private final AtomicLong counter = new AtomicLong();
+
+      /*
+       *curl localhost:8080/greeting
+       @return={id =1 , content="hello world!}
+       * localhost:8080/greeting?name=shilpa
+       * @return= { id=2, content="hello shilpa!
+       */
+      @GetMapping(value={"/greeting","/greeting/","/greeting/home"})
+      public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+          return new Greeting(counter.incrementAndGet(), String.format(template, name));
+      }
+      /*
+      *localhost:8080/greeting/Anant
+      @return={id =1 , content="hello Anant!}
+       */
+      @GetMapping("greeting/{name}")
+      public Greeting greetings(@PathVariable String name) {
+          return new Greeting(counter.incrementAndGet(), String.format(template, name));
+      }
+
+}
